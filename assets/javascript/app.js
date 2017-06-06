d0cf813a4e136ea5609d3d65e7e6eea4fb429819
Tamara Tangney <tamaratangney@tamaras-mbp.ittns.northwestern.edu>
2017-06-06 17:09:08 -0500

    add

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -197,16 +197,9 @@ tally_score: function(){
 
 },
 
-new_game: function(){
-
- 	location.reload();
-  TriviaGame.start();
-
- },
 };
 
  $("#start").on("click", TriviaGame.start);
- $("#reset").on("click", TriviaGame.new_game);
  $("#button1").click(TriviaGame.checkanswer);
  $("#button2").click(TriviaGame.checkanswer);
  $("#button3").click(TriviaGame.checkanswer);
